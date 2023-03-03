db071c64adb0428c8d1b75abe54c5c63774982c5
강진현 <kangjin627@gmail.com>
2023-03-03 09:33:19 +0900

    [FEAT] 로그인/회원가입 퍼블리싱 완성

--- memoir/src/page/signup/styles.js
+++ memoir/src/page/signup/styles.js
@@ -1,32 +1,29 @@
 import styled from "styled-components";
 import { Color, MainColor } from "../../interface";
 
-export const LoginFlex = styled.div`
-  height: 80%;
+
+export const SignupFlex = styled.div`
+  height: 85%;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export const LoginWarpper = styled.div`
+export const SignupWarpper = styled.div`
   background-color: ${Color.HeaderInputBorder};
   width: 400px;
-  height: 450px;
+  height: 500px;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  & > h1 {
-    margin: 30px 0 5px 0;
-  }
-
-  & > p {
-    margin-bottom: 50px;
+  & > h1{
+    margin: 30px 0 30px 0;
   }
 `;
 
-export const LoginInput = styled.input`
+export const SignupInput = styled.input`
   width: 350px;
   height: 50px;
   margin-bottom: 15px;
@@ -42,7 +39,7 @@ export const LoginInput = styled.input`
   font-size: 16px;
 `;
 
-export const LoginBtn = styled.button`
+export const SignupBtn = styled.button`
   width: 350px;
   height: 50px;
   font-size: 18px;
@@ -52,21 +49,4 @@ export const LoginBtn = styled.button`
   background-color: ${MainColor};
   border: transparent;
   color: white;
-`;
-
-export const SignupBtn = styled.button`
-  width: 350px;
-  height: 50px;
-  background-color: transparent;
-  font-size: 18px;
-  font-weight: bold;
-  border: 1px solid transparent;
-  border-radius: 8px;
-  margin-top: 10px;
-  color: ${Color.LoginInputFont};
-  &:hover {
-    border: 4px solid ${Color.HeaderInputBorder};
-    font-size: 20px;
-    transition: all 0.3s;
-  }
 `;
